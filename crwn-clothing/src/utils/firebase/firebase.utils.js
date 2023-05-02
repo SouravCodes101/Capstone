e86bdae2b6386b63899504210cee5b99101d2ddd
Sourav <sourav.choudhury@positra.com>
2023-05-02 11:25:58 +0530

    FireStore init

--- crwn-clothing/src/utils/firebase/firebase.utils.js
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -6,6 +6,13 @@ import {
   GoogleAuthProvider 
 } from "firebase/auth";
 
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+} from "firebase/firestore"
+
 const firebaseConfig = {
   apiKey: "AIzaSyAVnT38jE6lmGZfVCVoOqXQRXtadqdD-iE",
   authDomain: "crwn-clothing-db-1a49f.firebaseapp.com",
@@ -26,3 +33,16 @@ provider.setCustomParameters ({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists()); 
+}
