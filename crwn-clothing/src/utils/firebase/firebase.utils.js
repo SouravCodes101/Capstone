a7a1786e6724890556707e77818985a4e8f50705
Sourav <sourav.choudhury@positra.com>
2023-05-02 10:58:13 +0530

    Firebase Google Sign In setup

---
+++ crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { 
+  getAuth, 
+  signInWithRedirect, 
+  signInWithPopup, 
+  GoogleAuthProvider 
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAVnT38jE6lmGZfVCVoOqXQRXtadqdD-iE",
+  authDomain: "crwn-clothing-db-1a49f.firebaseapp.com",
+  projectId: "crwn-clothing-db-1a49f",
+  storageBucket: "crwn-clothing-db-1a49f.appspot.com",
+  messagingSenderId: "107621748061",
+  appId: "1:107621748061:web:bdc42c82c90ee65430ecb4"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters ({
+  prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
